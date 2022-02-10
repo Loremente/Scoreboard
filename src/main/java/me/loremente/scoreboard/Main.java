f1af91ec72d3f5252639b07d1674cc2be5567850
Lorenzo <Loremente@outlook.it>
2022-02-10 21:42:36 +0100

    Main class

--- src/main/java/me/loremente/scoreboard/Main.java
+++ src/main/java/me/loremente/scoreboard/Main.java
@@ -1,12 +1,15 @@
 package me.loremente.scoreboard;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scoreboard.DisplaySlot;
 import org.bukkit.scoreboard.Objective;
+import org.bukkit.scoreboard.Score;
 import org.bukkit.scoreboard.Scoreboard;
 
 public final class Main extends JavaPlugin implements Listener {
@@ -23,7 +26,12 @@ public final class Main extends JavaPlugin implements Listener {
 
         Scoreboard board = Bukkit.getScoreboardManager().getNewScoreboard();
 
-        Objective obj = board.registerNewObjective("","");
+        Objective obj = board.registerNewObjective("testboard","dummy");
+        obj.setDisplaySlot(DisplaySlot.SIDEBAR);
+        obj.setDisplayName("testboard");
+
+        Score website = obj.getScore(ChatColor.YELLOW + "www.stocazzo.com");
+        website.setScore();
 
         player.setScoreboard(board);
 
