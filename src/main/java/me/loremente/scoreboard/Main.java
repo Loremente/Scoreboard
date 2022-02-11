07c2e49a6c4bdfb0ecfb2c37febbc52f465a290d
Lorenzo <Loremente@outlook.it>
2022-02-11 17:56:06 +0100

    Main class

--- src/main/java/me/loremente/scoreboard/Main.java
+++ src/main/java/me/loremente/scoreboard/Main.java
@@ -1,46 +1,13 @@
 package me.loremente.scoreboard;
 
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.scoreboard.*;
 
-public final class Main extends JavaPlugin implements Listener {
+public final class Main extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        Bukkit.getPluginManager().registerEvents(this,this);
-    }
-
-    @EventHandler
-    public void onJoin(PlayerJoinEvent e) {
-
-        Player player = e.getPlayer();
-
-        Scoreboard board = Bukkit.getScoreboardManager().getNewScoreboard();
-
-        Objective obj = board.registerNewObjective("testboard","dummy");
-        obj.setDisplaySlot(DisplaySlot.SIDEBAR);
-        obj.setDisplayName(ChatColor.GREEN + "Scoreboard");
-
-        Score website = obj.getScore(ChatColor.YELLOW + "www.stocazzo.com");
-        website.setScore(1);
-
-        Score space = obj.getScore("");
-        space.setScore(2);
-
-        Team blocksBroken = board.registerNewTeam("blocchirotti");
-        blocksBroken.addEntry(ChatColor.BOLD.toString());
-        blocksBroken.setPrefix(ChatColor.BLUE + "Blocchi rotti: " );
-        blocksBroken.setSuffix(ChatColor.YELLOW + "0");
-        obj.getScore(ChatColor.BOLD.toString()).setScore(3);
-
-        player.setScoreboard(board);
-
+        Bukkit.getPluginManager().registerEvents(new SidebarListener(),this);
     }
 
 }
